b64d0de6de081a5bf4875ec89d91cd9b536bcee5
The Jody Parsley Legacy Organization <firstrapper@jodyparsley.org>
2026-06-06 02:16:56 -0400

    Update injection.js

--- injection.js
+++ injection.js
@@ -1,5 +1,5 @@
 if (window.self === window.top) {
     document.body.insertAdjacentHTML("afterbegin", `
-        Brought to you by <abbr title="The Jody Parsley Legacy Organization"><b><line><a href="https://www.jodyparsley.org" target="_parent">J.P. Legacy Org.</a></line></b></abbr><img src="../../../favicon.ico" width="24px" style="box-shadow: none; border: none;">
+        Brought to you by <abbr title="The Jody Parsley Legacy Organization"><b><line><a href="https://www.jodyparsley.org" target="_parent" style="text-decoration: none; color: black;">J.P. Legacy Org.</a></line></b></abbr><img src="https://www.jodyparsley.org/favicon.ico" width="24px" style="box-shadow: none; border: none;">
     `);
 }
